761af3bace830febf4ffc39068e7b8f1af5b0800
Edujvr <32462670+Edujvr@users.noreply.github.com>
2019-03-29 16:51:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.post("/webhook",(req, res) =>{
   //console.log(req.body.queryResult.outputContexts);	
   var contextos = req.body.queryResult.outputContexts;
   var i,len = contextos.length
-  //console.log(len);
+  console.log(req.body);
   //console.log(contextos);
 	
 	for(i=0;i<len;i++){
