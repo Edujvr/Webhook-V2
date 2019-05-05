da8d69149e4d623160d0ce3049642192dde3bd6c
Edujvr <32462670+Edujvr@users.noreply.github.com>
2019-05-04 21:53:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ require("./config/db");
 //Creación del metodo que escucha las llamadas POST y obtiene los parametros
 
 app.post("/webhook",(req, res) =>{   
-  console.log(req.body.queryResult.fulfillmentMessages);	
+  console.log(req.body.queryResult.fulfillmentMessages.text.text[1]);	
   const action = req.body.queryResult.action;
   const chatbase = require('@google/chatbase');
   const chatbase2= require('@google/chatbase');
