3d5da7d5f618098d8cc6e3e79478631eebe88a0d
Edujvr <32462670+Edujvr@users.noreply.github.com>
2019-05-27 11:40:07 -0500

    Create Reclamos.js

---
+++ models/Reclamos.js
@@ -0,0 +1,16 @@
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ReclamoSchema = new Schema({
+  SUBTIPO:{
+    type: String,
+    required:true
+  },
+  VENTANA DE AYUDA: {
+    type: String,
+    required:true
+  }
+});
+
+module.exports = mongoose.model("Reclamo", ReclamoSchema);
