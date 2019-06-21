f5888c5d9f62abeb3ad9b35c50df922e9941a59c
Edujvr <32462670+Edujvr@users.noreply.github.com>
2019-06-21 10:55:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ app.post("/webhook",(req, res) =>{
 			    if (err) {
 			      res.status(500).send(err);
 			    }
-				respuestaBot = reclamos.AYUDA +"\n Para más información ingresa en el siguiente link: http://bit.ly/2IRBCzG "
+				respuestaBot = reclamos.AYUDA +"\n\nPara más información ingresa en el siguiente link: http://bit.ly/2IRBCzG "
 				console.log(respuestaBot)
 				sendResponse(respuestaBot);
 				sendAnalytics(nameW);
