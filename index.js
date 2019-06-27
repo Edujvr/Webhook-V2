885d27d3473b376722292992b7bef251da9644cc
Edujvr <32462670+Edujvr@users.noreply.github.com>
2019-06-27 17:42:07 -0500

    Update index.js

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ app.post("/webhook",(req, res) =>{
 			query.findOne(function (err, agencias) {
 			    if (err) {
 			      res.status(500).send(err);
-			    }	respuestaBot = "La Agencia " + agencias.NOMBRE + " se encuentra en: \n" + agencias.PROVINCIA + "- " + agencias.CIUDAD + ", " + agencias.DIRECCION + "\nReferencia: " + agencias.REFERENCIA + "\nTeléfonos: " + agencias.TELF_1 + " /" + agencias.TELF_2 + "\nHorarios \n Semana: " + agencias.H_SEMANA + "\n Sábado: " + agencias.H_SABADO + "\n Domingo: " + agencias.H_DOMINGO
+			    }	respuestaBot = "La Agencia " + agencias.NOMBRE + " cc: "+agencias.CC + " se encuentra en: \n" + agencias.PROVINCIA + "- " + agencias.CIUDAD + ", " + agencias.DIRECCION + "\nReferencia: " + agencias.REFERENCIA + "\nTeléfonos: " + agencias.TELF_1 + " /" + agencias.TELF_2 + "\nHorarios \n Semana: " + agencias.H_SEMANA + "\n Sábado: " + agencias.H_SABADO + "\n Domingo: " + agencias.H_DOMINGO
 				sendResponse(respuestaBot);
 				sendAnalytics(nameW);
 			  });
