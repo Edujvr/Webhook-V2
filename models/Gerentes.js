3d37c9973b8d20eba7b00b93c50f7c41e5c153ea
Edujvr <32462670+Edujvr@users.noreply.github.com>
2019-06-28 10:40:02 -0500

    Update Gerentes.js

--- models/Gerentes.js
+++ models/Gerentes.js
@@ -5,30 +5,6 @@ const GerentesSchema = new Schema({
   NOMBRE:{
     type: String,
     required:true
-  },
-  NOMBRE_REGIONAL_CANALES_SERV:{
-    type: String,
-    required:true
-  },
-  CEL_NOMBRE_REGIONAL_CANALES_SERV:{
-    type: String,
-    required:true
-  },
-  EXT_NOMBRE_REGIONAL_CANALES_SERV:{
-    type: String,
-    required:true
-  },  
-  NOMBRE_REGIONAL_NEGOCIO:{
-    type: String,
-    required:true
-  },
-  CEL_NOMBRE_REGIONAL_NEGOCIO:{
-    type: String,
-    required:true
-  },
-  EXT_NOMBRE_REGIONAL_NEGOCIO:{
-    type: String,
-    required:true
   },  
   GERENTE_AGENCIA:{
     type: String,
