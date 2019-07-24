17a61703c26960ef376d7675ee86a5a7472a236f
Edujvr <32462670+Edujvr@users.noreply.github.com>
2019-07-24 11:27:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -93,10 +93,15 @@ app.post("/webhook",(req, res) =>{
 			console.log(colaboradores)
 			   if (err) {
 			      res.status(500).send(err);
-			    }if(colaboradores.PUESTO =='EJECUTIVO SERVICIOS TRANSACCIONALES' || colaboradores.PUESTO =='EJECUTIVO SERVICIOS TRANSACCIONALES SR' || colaboradores.PUESTO =='ESPECIALISTA INTELIGENCIA DE NEGOCIOS')	
+			    }if(colaboradores.PUESTO =='EJECUTIVO SERVICIOS TRANSACCIONALES' || colaboradores.PUESTO =='EJECUTIVO SERVICIOS TRANSACCIONALES SR' || colaboradores.PUESTO =='ESPECIALISTA INTELIGENCIA DE NEGOCIOS'){	
 				respuestaBot = "Lamentamos la Baja de " + colaboradores.NOMBRE
 				sendResponse(respuestaBot);
 				sendAnalytics(nameW);
+			    }else if{
+			    	respuestaBot = "Lo sentimos, no es posible procesar tu pedido; posiblemente la persona reportada no es Ejecutivo de Servicios Transaccionales o tú no estás registrado como su línea de supervisión. Por favor toma contacto con tu generalista "
+				sendResponse(respuestaBot);
+				sendAnalytics(nameW);
+			    }
 			  });
 			
 		});	
