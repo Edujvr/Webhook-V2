c3dbe368a615f23755d70788cb84738f387e6d58
Edujvr <32462670+Edujvr@users.noreply.github.com>
2019-07-25 16:03:54 -0500

    Create SalidasCajeros.js

---
+++ models/SalidasCajeros.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const SalidaCajeroSchema = new Schema({
+  createdOn: {
+        type: Date,
+        default: Date.now
+  },
+  SesionId:{
+      type: String,
+      required:true,
+  },
+  IdLS:{
+        type: Number,
+        required:true
+  },
+  NombreLS:{
+        type: String,
+        required:true
+  },
+  CorreoLS:{
+        type: String,
+        required:true
+  },
+  IdCajero:{
+        type: String,
+        required:true
+  },
+  MotivoSalida:{
+        type: String,
+        required:true
+  },
+  FechaSalida:{
+        type: String,
+        required:true
+  },
+  AdjCartaRenuncia:{
+        type: String,
+        required:true
+  },
+  AdjFormularioSalida: {
+        type: String,
+        required:true
+  }
+});
+
+module.exports = mongoose.model("SalidaCajero", SalidaCajeroSchema);
