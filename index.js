36af3495005af1d48456818c9b7a684952dd802f
Edujvr <32462670+Edujvr@users.noreply.github.com>
2019-08-01 17:49:56 -0500

    Update index.js

--- index.js
+++ index.js
@@ -165,10 +165,11 @@ app.post("/webhook",(req, res) =>{
 			nameW=res.name;
 			email=res.email;
 			var fecha = String(req.body.queryResult.parameters.date)
-			var miFechaSalida = new Date(fecha.substr(0,3),fecha.substr(5,6),fecha.substr(8,9),0,0,0)
-			console.log(fecha.substr(0,3))
-			console.log(fecha.substr(5,6))
-			console.log(fecha.substr(8,9))
+			var miFechaSalida = new Date(fecha.substr(0,4),fecha.substr(5,1),fecha.substr(8,1),0,0,0)
+			console.log(fecha)
+			console.log(fecha.substr(0,4))
+			console.log(fecha.substr(5,1))
+			console.log(fecha.substr(8,1))
 			//console.log(miFechaSalida)
 			var miFechaActual = new Date()
 			miFechaActual.setDate(miFechaActual.getDate() + 15);
