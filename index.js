c0d1c792aeb31afba2351d8c19b5ff9fa9c98f82
Edujvr <32462670+Edujvr@users.noreply.github.com>
2019-08-01 17:41:07 -0500

    Update index.js

--- index.js
+++ index.js
@@ -164,7 +164,7 @@ app.post("/webhook",(req, res) =>{
 	 	graph.get(id+"?fields=name,email", function(err, res){
 			nameW=res.name;
 			email=res.email;
-			var fecha = req.body.queryResult.parameters.date
+			var fecha = String(req.body.queryResult.parameters.date)
 			var miFechaSalida = new Date(fecha.substr(0,3),fecha.substr(5,6),fecha.substr(8,9),0,0,0)	
 			console.log(miFechaSalida.getDate())
 			var miFechaActual = new Date()
