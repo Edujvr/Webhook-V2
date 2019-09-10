b5b03940aacc13aca74e0f26413323a9eebba4d6
Edujvr <32462670+Edujvr@users.noreply.github.com>
2019-09-10 14:19:04 -0500

    Update Reclamos.js

--- models/Reclamos.js
+++ models/Reclamos.js
@@ -3,6 +3,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ReclamoSchema = new Schema({
+  TIPOA:{
+    type: String,
+    required:true
+  },
   TIPO:{
     type: String,
     required:true
