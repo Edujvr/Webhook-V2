62675d7e69f111a10f5d52a9e77d08e988f48ce3
Edujvr <32462670+Edujvr@users.noreply.github.com>
2019-09-12 10:21:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -277,7 +277,7 @@ app.post("/webhook",(req, res) =>{
 			FechaActualMin.setDate(FechaActualMin.getDate() - 15);
 			console.log(FechaActualMin)
 			if(FechaSalida <= FechaActualMax && FechaSalida >= FechaActualMin){
-				respuestaBot='Ahora por favor toma foto a la hoja de salida que te proporcionamos arriba y cárgala en este feed.'
+				respuestaBot='Ahora por 1 favor toma foto a la hoja de salida que te proporcionamos arriba y cárgala en este feed.'
 				respuesta=getContext(CausaSalida,respuestaBot);
 			}else{
 				respuestaBot='Lo siento 1 la fecha ingresada no esta en el rango permitido para notificar la salida, por favor comunicarse con su generalista'  
