820f7834eccf3d1207d21784fc8bf8744b287c23
Edujvr <32462670+Edujvr@users.noreply.github.com>
2019-09-27 15:18:13 -0500

    Update index.js

--- index.js
+++ index.js
@@ -356,15 +356,15 @@ app.post("/webhook",(req, res) =>{
 					if (err) {
 					  res.status(500).send(err);
 					}	respuestaBot = "La Agencia " + agencias.NOMBRE + " cc: "+ agencias.CC + " se encuentra en: \n" + agencias.PROVINCIA + "- " + agencias.CUIDAD + ", " + agencias.DIRECCION + "\nReferencia: " + agencias.REFERENCIA + "\nTeléfonos: " + agencias.TELF_1 + " /" + agencias.TELF_2 + "\nHorarios \n Lunes a Viernes: " + agencias.H_SEMANA + "\n Sábado: " + agencias.H_SABADO + "\n Domingo: " + agencias.H_DOMINGO
-				//var query1  = Gerentes.where({ NOMBRE: req.body.queryResult.parameters.NombreAgencia});	
+				var query1  = Gerentes.where({ NOMBRE: req.body.queryResult.parameters.JefaturaNombreAgencia.NombreAgencia});	
 					/////////////////////
-					/* query1.findOne(function (err, gerentes) {
+					 query1.findOne(function (err, gerentes) {
 					//respuestaBot="Agencia "+ gerentes.NOMBRE+":";
 					if (err) {
 					  res.status(500).send(err);
 					}respuestaBot = respuestaBot+"\nGerente Agencia: " + gerentes.GERENTE_AGENCIA + "\nCEL: " + gerentes.CEL_GERENTE_AGENCIA + "\nEXT: " + gerentes.EXT_GERENTE_AGENCIA;
 							
-					var query2  = Administradores.where({ NOMBRE: req.body.queryResult.parameters.NombreAgencia});
+					var query2  = Administradores.where({ NOMBRE: req.body.queryResult.parameters.JefaturaNombreAgencia.NombreAgencia});
 					query2.findOne(function (err, administradores) {
 						respuestaBot=respuestaBot+"\n"
 						if (err) {
@@ -379,7 +379,7 @@ app.post("/webhook",(req, res) =>{
 							respuestaBot =respuestaBot+"\nEspecialista Comercial y Servicios: " + administradores.ESPECIALISTA_COMERCIAL_SERVICIOS + "\nCEL: " + administradores.CEL_ESPECIALISTA_COMERCIAL_SERVICIOS + "\nEXT: " + administradores.EXT_ESPECIALISTA_COMERCIAL_SERVICIOS;
 						}
 					
-					*/
+					
 					///////////////////////
 					sendResponse(respuestaBot);
 					sendAnalytics(nameW);
