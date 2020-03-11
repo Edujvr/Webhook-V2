dfd2e9f5888585c3f87b15e219f4bf95087c5395
Edujvr <32462670+Edujvr@users.noreply.github.com>
2020-03-11 11:29:08 -0500

    Update graphFB.js

--- functions/graphFB.js
+++ functions/graphFB.js
@@ -1,7 +1,7 @@
 var graph = require('fbgraph');
 const access_token = 'DQVJ2eHdaZAjV5a1dOdGVLNjN1TG9xeEZAhZAlRQOGRvN3dzRXd6SHFCTXN5X3J5T29fazVxNXFJOFFOel9QenhaYVVOM2g0ZADM2MnBlSGdzZAG1Ra2g5eXo5NGJ5a2FraE45Um5mZAmpuMXR6LU4zVV81ZAkRPbndsb1pfSDVwQXJVaWVXVUZAYcDJkNVpHTHgyQjBsMy1qZA1F0UVN6ZAjhFandzYXpnR01qQWlQa2FCZAHAzZA2h2WUtSc2lhWEphYko4S0V1TGpGbTRJb0JkQjRPU216MwZDZD';
 module.exports = {
-	async function graphID(id){
+	async graphID(id){
 		graph.setAccessToken(access_token);
 		const data = await graph.get(id+"?fields=name,email", function(err, res){	
 		}return data;			     
