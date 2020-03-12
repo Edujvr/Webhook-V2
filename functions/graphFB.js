0967d68f806481807a37c288337b2eacd37e8e46
Edujvr <32462670+Edujvr@users.noreply.github.com>
2020-03-12 00:03:29 -0500

    Update graphFB.js

--- functions/graphFB.js
+++ functions/graphFB.js
@@ -1,10 +1,15 @@
 var graph = require('fbgraph');
+var Q = require('q');
 const access_token = 'DQVJ2eHdaZAjV5a1dOdGVLNjN1TG9xeEZAhZAlRQOGRvN3dzRXd6SHFCTXN5X3J5T29fazVxNXFJOFFOel9QenhaYVVOM2g0ZADM2MnBlSGdzZAG1Ra2g5eXo5NGJ5a2FraE45Um5mZAmpuMXR6LU4zVV81ZAkRPbndsb1pfSDVwQXJVaWVXVUZAYcDJkNVpHTHgyQjBsMy1qZA1F0UVN6ZAjhFandzYXpnR01qQWlQa2FCZAHAzZA2h2WUtSc2lhWEphYko4S0V1TGpGbTRJb0JkQjRPU216MwZDZD';
 graph.setAccessToken(access_token);
 module.exports = {
 	async graphID(id){
+		var deferred = Q.deferred();
 		graph.get(id+"?fields=name,email",async function(err, res){
-			await res.send(res)
+			//await res.send(res)
+			if (err) deferred.reject(err);
+                	deferred.resolve(res);
 		});
+		return deferred.promise
 	}
 };
