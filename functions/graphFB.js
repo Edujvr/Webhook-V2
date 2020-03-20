fe126902f1d1fa4fb7c9827299c0598e3adabf48
Edujvr <32462670+Edujvr@users.noreply.github.com>
2020-03-20 13:59:23 -0500

    Update graphFB.js

--- functions/graphFB.js
+++ functions/graphFB.js
@@ -1,11 +1,15 @@
-var graph = require('fbgraph');
-var Q = require('q');
+//var graph = require('fbgraph');
+const FacebookGraph = require('facebookgraph');
 const access_token = 'DQVJ2eHdaZAjV5a1dOdGVLNjN1TG9xeEZAhZAlRQOGRvN3dzRXd6SHFCTXN5X3J5T29fazVxNXFJOFFOel9QenhaYVVOM2g0ZADM2MnBlSGdzZAG1Ra2g5eXo5NGJ5a2FraE45Um5mZAmpuMXR6LU4zVV81ZAkRPbndsb1pfSDVwQXJVaWVXVUZAYcDJkNVpHTHgyQjBsMy1qZA1F0UVN6ZAjhFandzYXpnR01qQWlQa2FCZAHAzZA2h2WUtSc2lhWEphYko4S0V1TGpGbTRJb0JkQjRPU216MwZDZD';
-graph.setAccessToken(access_token);
+//graph.setAccessToken(access_token);
+const graph = new FacebookGraph(access_token)
+
 module.exports = {
 	async graphID(id){
-		graph.get(id+"?fields=name,email", function(err, res, next){
+		const zuck = await graph.get(id);
+		console.log(zuck)
+		/*graph.get(id+"?fields=name,email", function(err, res, next){
 			res.send('name');
-		});
+		});*/
 	}
 };
