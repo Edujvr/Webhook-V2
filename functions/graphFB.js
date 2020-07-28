b9110994eb54dacb51e1a3744309b09d81dc9fb0
Edujvr <32462670+Edujvr@users.noreply.github.com>
2020-07-27 20:17:54 -0500

    Update graphFB.js

--- functions/graphFB.js
+++ functions/graphFB.js
@@ -1,17 +1,17 @@
-var graph = require('fbgraph');
-//const FacebookGraph = require('facebookgraph');
+//var graph = require('fbgraph');
+const FacebookGraph = require('facebookgraph');
 const access_token = 'DQVJ2NUhWeU1kOW5YT0xEQUFYdkkyR3pUUVgydFhieFUxaXpoa3ZAvbERaOXYxNHcxa2Rmbm5tWmRjMm1veVhRWC1Db1hablN2ek1mMlI4dXhzNlJ6ZATZAyV3p4bllMcDZAXM3BkUW02VmhxbEVGV2tmSnhoQ2o5YmdlSUZAjdW9sM0xkMS10NllHZAzdfWExidGJDUnhjMEN6UFVLSjU1ZA29uNnlsbGduRjYzS3RFZAzFxYkhLUDFKRWJQTWRvWndGNmtteUZA4eUpsNEp1RWdSckZAPMQZDZD';
-graph.setAccessToken(access_token);
-//const graph = new FacebookGraph(access_token)
+//graph.setAccessToken(access_token);
+const graph = new FacebookGraph(access_token)
 
 module.exports = {
 	async graphID(id){
-		//const zuck = await graph.get(id+"?fields=name,email");
-		//console.log(zuck);
-		//return zuck;
-		graph.get(id+"?fields=name,email", function(err, res){
+		const zuck = await graph.get(id+"?fields=name,email");
+		console.log(zuck);
+		return zuck;
+		/*graph.get(id+"?fields=name,email", function(err, res){
 			console.log(res.name);
 			console.log(res.email)
-		});
+		});*/
 	}
 };
