458f574245137d0f5209131fb6952ac03719a012
Edujvr <32462670+Edujvr@users.noreply.github.com>
2020-07-28 19:24:14 -0500

    Update index.js

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ app.post("/webhook",async(req, res) =>{
 		});
 	}else if(action == "prueba"){
 		console.log("Entro en la validación")
-		console.log(req.body)
+		console.log(req.body.originalDetectIntentRequest)
 		//console.log(id)
 		/*graph.get(id+"?fields=name,email,first_name", function(err, res){
 			nameW=res.name
