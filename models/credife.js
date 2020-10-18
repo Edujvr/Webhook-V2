50b6dce25258e0112dff8d54003607f3dc04ab68
Edujvr <32462670+Edujvr@users.noreply.github.com>
2020-10-18 18:36:26 -0500

    Create credife.js

---
+++ models/credife.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const CredifeSchema = new Schema({
+  EMAIL: {
+    type: String,
+    required:true
+  },
+  CLIENTES: {
+    type: Array,
+    required:true
+  }
+});
+
+module.exports = mongoose.model("Objetivos", CredifeSchema);
