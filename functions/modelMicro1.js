0b740039bc805645a0904d5465998cc44762cedc
Edujvr <32462670+Edujvr@users.noreply.github.com>
2020-10-19 11:44:39 -0500

    Update modelMicro1.js

--- functions/modelMicro1.js
+++ functions/modelMicro1.js
@@ -1,35 +1,89 @@
 module.exports = {
-  async modGeneralista(msj1,msj2){
+  async modMicro1(){
       let respuesta ={
        "fulfillmentText":msj1 + msj2,
        "fulfillmentMessages":[
-          {
-             "text":{
-                "text":[
-                   msj1
-                ]
-             },
-             "platform":"FACEBOOK",
-             "lang":"es"
+        {
+          "text": {
+            "text": [
+              "Bienvenid@ al piloto de Estrategias de cobranzas. Gracias por participar, tus respuestas nos ayudarán muchisimo."
+            ]
           },
-          {
-             "text":{
-                "text":[
-                   msj2
-                ]
-             },
-             "platform":"FACEBOOK",
-             "lang":"es"
+          "platform": "FACEBOOK"
+        },
+        {
+          "text": {
+            "text": [
+              "Mi compañera Elizabeth te explicará cómo funciona este piloto con el siguiente video."
+            ]
           },
-          {
-             "text":{
-                "text":[
-                   msj1 + msj2
-                ]
-             },
-             "lang":"es"
+          "platform": "FACEBOOK"
+        },
+        {
+          "payload": {
+            "facebook": {
+              "attachment": {
+                "payload": {
+                  "url": "https://storage.googleapis.com/videos_pibot/video-1556059174.mp4"
+                },
+                "type": "video"
+              }
+            }
+          },
+          "platform": "FACEBOOK"
+        },
+        {
+          "text": {
+            "text": [
+              "¿List@ para empezar?"
+            ]
+          },
+          "platform": "FACEBOOK"
+        },
+        {
+          "payload": {
+            "facebook": {
+              "attachment": {
+                "payload": {
+                  "template_type": "list",
+                  "elements": [
+                    {
+                      "buttons": [
+                        {
+                          "type": "postback",
+                          "title": "SI",
+                          "payload": "Si, empezar"
+                        }
+                      ],
+                      "title": "Si, empezar"
+                    },
+                    {
+                      "buttons": [
+                        {
+                          "type": "postback",
+                          "title": "NO",
+                          "payload": "No, no me queda claro lo que tengo que hacer"
+                        }
+                      ],
+                      "title": "No, no me queda claro lo que tengo que hacer"
+                    }
+                  ],
+                  "top_element_style": "compact"
+                },
+                "type": "template"
+              }
+            }
+          },
+          "platform": "FACEBOOK"
+        },
+        {
+          "text": {
+            "text": [
+              "Bienvenid@ al piloto de Estrategias de cobranzas. Gracias por participar, tus respuestas nos ayudarán muchisimo.\nMi compañera Elizabeth te explicará cómo funciona este piloto con el siguiente video.\nVideo.\n¿List@ para empezar?[SI\NO]"
+            ]
           }
-       ]
+        }
+      ]
     }
       return respuesta;
   } 
