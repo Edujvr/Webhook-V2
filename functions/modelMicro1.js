9874f5d64a9ee0cc0cb6cd3d16360e4afcb089f1
Edujvr <32462670+Edujvr@users.noreply.github.com>
2020-10-19 17:12:53 -0500

    Update modelMicro1.js

--- functions/modelMicro1.js
+++ functions/modelMicro1.js
@@ -24,7 +24,7 @@ module.exports = {
             "facebook": {
               "attachment": {
                 "payload": {
-                  "url": "https://storage.googleapis.com/videos_pibot/video-1556059174.mp4"
+                  "url": ""//"https://storage.googleapis.com/videos_pibot/video-1556059174.mp4"
                 },
                 "type": "video"
               }
