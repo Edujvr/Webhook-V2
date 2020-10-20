7faa89b9bab7dc4edd327acdd97edc34979c9072
Edujvr <32462670+Edujvr@users.noreply.github.com>
2020-10-20 11:45:13 -0500

    Update modelMicro5.js

--- functions/modelMicro5.js
+++ functions/modelMicro5.js
@@ -1,5 +1,5 @@
 module.exports = {
-  async modMicro4(frase){
+  async modMicro5(frase){
       let respuesta ={
        "fulfillmentText":"Bienvenid@ al piloto de Estrategias de cobranzas. Gracias por participar, tus respuestas nos ayudarán muchisimo.\nMi compañera Elizabeth te explicará cómo funciona este piloto con el siguiente video.\nVideo.\n¿List@ para empezar?[SI\NO]",
        "fulfillmentMessages":[
