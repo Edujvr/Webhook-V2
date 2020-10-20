fe52a623e38271e63e0b6d4dbec788ff46996f78
Edujvr <32462670+Edujvr@users.noreply.github.com>
2020-10-19 19:43:37 -0500

    Create modelMicro3.js

---
+++ functions/modelMicro3.js
@@ -0,0 +1,27 @@
+module.exports = {
+  async modMicro2(ejecutivo,cliente){
+      let respuesta ={
+       "fulfillmentText":"Bienvenid@ al piloto de Estrategias de cobranzas. Gracias por participar, tus respuestas nos ayudarán muchisimo.\nMi compañera Elizabeth te explicará cómo funciona este piloto con el siguiente video.\nVideo.\n¿List@ para empezar?[SI\NO]",
+       "fulfillmentMessages":[
+        {
+          "quickReplies": {
+            "title": "¿Necesitas información adicional del cliente para ejecutar una estrategia de cobranza?",
+            "quickReplies": [
+              "SI",
+              "NO"
+            ]
+          },
+          "platform": "FACEBOOK"
+        },
+        {
+          "text": {
+            "text": [
+              "ok"
+            ]
+          }
+        }
+      ]
+    }
+      return respuesta;
+  } 
+}
