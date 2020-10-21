191221fa3a46e746d86b8f590b4e83c2875068c9
Edujvr <32462670+Edujvr@users.noreply.github.com>
2020-10-21 11:02:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -162,13 +162,13 @@ app.post("/webhook",async(req, res) =>{
 			}else{
 				const num = await numCliente(microfinanzas)
 				console.log(num)
-				if(num === 19){
-					//console.log("Entro 1")
+				if(num === 20){
+					console.log("Entro 1")
 					respuesta = await modMicro3();
 					sendResponse(respuesta);
 					sendAnalytics(nameW);
 				}else{
-					//console.log("Entro 2")
+					console.log("Entro 2")
 					respuesta = await modMicro4();
 					sendResponse(respuesta);
 					sendAnalytics(nameW);
