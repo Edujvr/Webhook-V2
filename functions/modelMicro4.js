f9ff3b35d493aa626fafcb1b211ab7f78a938b10
Edujvr <32462670+Edujvr@users.noreply.github.com>
2020-10-22 23:04:24 -0500

    Update modelMicro4.js

--- functions/modelMicro4.js
+++ functions/modelMicro4.js
@@ -6,7 +6,7 @@ module.exports = {
         {
           "text": {
             "text": [
-              "Cuéntanos, qué estrategia de cobranza aplicarías con este cliente:\n1.- No requiere gestión\n2.- Mensaje preventivo cliente buen perfil\n3.- Mensaje preventivo cliente mal perfil\n4.- Mensaje preventivo reestructurado\n5.- Mensaje cliente con mora\n6.- Llamada cliente\n7.- Llamada garante o familiar\n8.- Visita clientes\n9.- Visita garante o familiar\n10.- Visita con administrador a cliente\n11.- Visita con administrador a garante o familiar\n12.- Otra"
+              "Ahora sí cuéntanos, qué estrategia de cobranza aplicarías con este cliente:\n1.- No requiere gestión\n2.- Mensaje preventivo cliente buen perfil\n3.- Mensaje preventivo cliente mal perfil\n4.- Mensaje preventivo reestructurado\n5.- Mensaje cliente con mora\n6.- Llamada cliente\n7.- Llamada garante o familiar\n8.- Visita clientes\n9.- Visita garante o familiar\n10.- Visita con administrador a cliente\n11.- Visita con administrador a garante o familiar\n12.- Otra"
             ]
           },
           "platform": "FACEBOOK"
