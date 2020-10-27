107ffa58b88c6c08cc58b518e34bb5b1f6aefbad
Edujvr <32462670+Edujvr@users.noreply.github.com>
2020-10-26 23:40:12 -0500

    Update modelMicro4b.js

--- functions/modelMicro4b.js
+++ functions/modelMicro4b.js
@@ -1,7 +1,7 @@
 module.exports = {
   async modMicro4b(){
       let respuesta ={
-       "fulfillmentText":"Bienvenid@ al piloto de Estrategias de cobranzas. Gracias por participar, tus respuestas nos ayudarán muchisimo.\nMi compañera Elizabeth te explicará cómo funciona este piloto con el siguiente video.\nVideo.\n¿List@ para empezar?[SI\NO]",
+       "fulfillmentText":"Gracias por tu feedback. Analizaremos la posibilidad de agregar este dato\nAhora sí cuéntanos, qué estrategia de cobranza aplicarías con este cliente:\n1.- No requiere gestión\n2.- Mensaje preventivo cliente buen perfil\n3.- Mensaje preventivo cliente mal perfil\n4.- Mensaje preventivo reestructurado\n5.- Mensaje cliente con mora\n6.- Llamada cliente\n7.- Llamada garante o familiar\n8.- Visita clientes\n9.- Visita garante o familiar\n10.- Visita con administrador a cliente\n11.- Visita con administrador a garante o familiar\n12.- Otra\nEscribe el número de  la estrategia que elegirías",
        "fulfillmentMessages":[
         {
           "text": {
@@ -30,7 +30,7 @@ module.exports = {
         {
           "text": {
             "text": [
-              "ok"
+              "Gracias por tu feedback. Analizaremos la posibilidad de agregar este dato\nAhora sí cuéntanos, qué estrategia de cobranza aplicarías con este cliente:\n1.- No requiere gestión\n2.- Mensaje preventivo cliente buen perfil\n3.- Mensaje preventivo cliente mal perfil\n4.- Mensaje preventivo reestructurado\n5.- Mensaje cliente con mora\n6.- Llamada cliente\n7.- Llamada garante o familiar\n8.- Visita clientes\n9.- Visita garante o familiar\n10.- Visita con administrador a cliente\n11.- Visita con administrador a garante o familiar\n12.- Otra\nEscribe el número de  la estrategia que elegirías"
             ]
           }
         }
