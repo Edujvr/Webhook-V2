0a9a87d092f0686e63aa72257f2cc9c2e169e7a5
Edujvr <32462670+Edujvr@users.noreply.github.com>
2020-10-28 00:51:37 -0500

    Update modelMicroParche.js

--- functions/modelMicroParche.js
+++ functions/modelMicroParche.js
@@ -1,8 +1,8 @@
 module.exports = {
-  async modMicro2(ejecutivo,cliente){
+  async modMicro2(ejecutivo,cliente,req){
       let respuesta ={
        "fulfillmentText":ejecutivo + ", aquí va tu " + cliente.OrdenCliente + " cliente\nNombre: " + cliente.NombreCliente +"*DATOS PERSONALES*\n\nEdad: "+ cliente.Edad + "\nEstado Civil: " + cliente.EstadoCivil+ "\nHijos: " + cliente.Hijos + "\nVivienda: " + cliente.Vivienda+"\n*DATOS ECONOMICOS*\n\nActividad económica: " + cliente.ActividadEconomica +"\nSector económico: " + cliente.SectorEconomico + "\nMora del sector económico: " + cliente.MoraSectorEconomicoDias +" días" + "\nCantidad de operaciones activas: " + cliente.CantidadOperacionesActivas + "\nMonto cuota: $" + cliente.MontoCuotaDolar + "\nNumero de cuota: " + cliente.NumCuota + "\nSaldo Insoluto: $" + cliente.SaldoInsolutoDolar + "\nCiclo: " + cliente.Ciclo + "\nMora actual: " + cliente.MoraActualDias + " días" + "\nFecha próximo pago: " + cliente.FechaProximoPago + "\nMora Histórica en los últimos 12 meses: " + cliente.MoraHistorica12Meses +" días" + "\nMora mes anterior: " + cliente.MoraMesAnterior +" días" + "\nReestructurado: " + cliente.Reestructurado+ "\n*FAMILIARES Y GARANTES*\n\nNombre Garante:" + cliente.NombreGarante + "\nSu garante es familiar: " + cliente.GaranteFamilia + "\nTipo de garantía: " + cliente.TipoGarantia + "\nNúmero de familiares con mora: " + cliente.NumeroFamiliaresMora + "\nNúmero de familiares con castigo: " + cliente.NumeroFamiliaresCastigo+"\nEscribe OK para continuar",
-       outputContexts : [{'name': req.body.session+'/contexts/bucle','lifespanCount':1}]
+       "outputContexts" : [{'name': req.body.session+'/contexts/bucle','lifespanCount':1}],
        "fulfillmentMessages":[
         {
           "text": {
