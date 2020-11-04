bfdc99824b639df7f670a69a9fc9d07b8cfb6b6c
Edujvr <32462670+Edujvr@users.noreply.github.com>
2020-11-04 16:17:30 -0500

    Update modelMicro2.js

--- functions/modelMicro2.js
+++ functions/modelMicro2.js
@@ -63,7 +63,7 @@ module.exports = {
           "quickReplies": {
             "title": "*FAMILIARES Y GARANTES*\n\nNombre Garante:" + cliente.NombreGarante + "\nSu garante es familiar: " + cliente.GaranteFamilia + "\nTipo de garantía: " + cliente.TipoGarantia + "\nNúmero de familiares con mora: " + cliente.NumeroFamiliaresMora + "\nNúmero de familiares con castigo: " + cliente.NumeroFamiliaresCastigo,
             "quickReplies": [
-              "Continuar"
+              "OK"
             ]
           },
           "platform": "FACEBOOK"
