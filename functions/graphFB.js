e0a279329f6d261714c5adfad32af3818021d733
Edujvr <32462670+Edujvr@users.noreply.github.com>
2020-11-05 19:26:14 -0500

    Update graphFB.js

--- functions/graphFB.js
+++ functions/graphFB.js
@@ -8,7 +8,12 @@ const graph = new FacebookGraph(access_token)
 module.exports = {
 	async graphID(id){
 		const zuck = await graph.get(id+"?fields=name,email");
-		console.log(zuck)
+		if(zuck==undefined){
+			zuck = 'nulo'
+			return zuck;
+		}else{
+			return zuck;
+		}
 		//	return zuck;
 			/*graph.get(id+"?fields=name,email", function(err, res){
 				console.log(res.name);
