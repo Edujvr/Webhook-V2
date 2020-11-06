37303473c226c881de7dd9ffb90ca0542e8be25f
Edujvr <32462670+Edujvr@users.noreply.github.com>
2020-11-05 23:10:55 -0500

    Update modelResEstra.js

--- functions/modelResEstra.js
+++ functions/modelResEstra.js
@@ -1,8 +1,7 @@
 
 module.exports = {
   async modResEstra(resp,req){
-      const aux= req.body.session
-      const aux2=aux.replace(/\//g ,'\\');
+    console.log(resp)
       let respuesta ={
        "fulfillmentText":  String(resp),
        "fulfillmentMessages":[
@@ -22,8 +21,7 @@ module.exports = {
           }
         }
       ],
-        "outputContexts" : [{'name': req.body.session+'/contexts/microfinanzas-inicio-yes-next-followup','lifespanCount':1},{'name': req.body.session+'/contexts/microfinanzas-clientecontinuar-followup','lifespanCount':1},{'name': req.body.session+'/contexts/otraestrategia','lifespanCount':1}]
-        // "outputContexts" : [{'name': aux2+'\contexts\microfinanzas-inicio-yes-next-followup','lifespanCount':1},{'name': aux2+'\contexts\microfinanzas-clientecontinuar-followup','lifespanCount':1},{'name': aux2+'\contexts\otraestrategia','lifespanCount':1}]
+      "outputContexts" : [{'name': req.body.session+'/contexts/microfinanzas-inicio-yes-next-followup','lifespanCount':1},{'name': req.body.session+'/contexts/microfinanzas-clientecontinuar-followup','lifespanCount':1},{'name': req.body.session+'/contexts/otraestrategia','lifespanCount':1}]
       }
       return respuesta;
   } 
