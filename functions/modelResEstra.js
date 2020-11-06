c55b558b045655c4b20b2c615264bf1a4f76ad7f
Edujvr <32462670+Edujvr@users.noreply.github.com>
2020-11-05 23:06:18 -0500

    Update modelResEstra.js

--- functions/modelResEstra.js
+++ functions/modelResEstra.js
@@ -22,7 +22,7 @@ module.exports = {
           }
         }
       ],
-        "outputContexts" : [{'name': req.body.session+'\contexts\microfinanzas-inicio-yes-next-followup','lifespanCount':1},{'name': req.body.session+'\contexts\microfinanzas-clientecontinuar-followup','lifespanCount':1},{'name': req.body.session+'\contexts\otraestrategia','lifespanCount':1}]
+        "outputContexts" : [{'name': req.body.session+'/contexts/microfinanzas-inicio-yes-next-followup','lifespanCount':1},{'name': req.body.session+'/contexts/microfinanzas-clientecontinuar-followup','lifespanCount':1},{'name': req.body.session+'/contexts/otraestrategia','lifespanCount':1}]
         // "outputContexts" : [{'name': aux2+'\contexts\microfinanzas-inicio-yes-next-followup','lifespanCount':1},{'name': aux2+'\contexts\microfinanzas-clientecontinuar-followup','lifespanCount':1},{'name': aux2+'\contexts\otraestrategia','lifespanCount':1}]
       }
       return respuesta;
