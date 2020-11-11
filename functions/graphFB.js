b617a54e86eb2f1a8b6cdd1b37960e2c7639e881
Edujvr <32462670+Edujvr@users.noreply.github.com>
2020-11-11 09:29:04 -0500

    Update graphFB.js

--- functions/graphFB.js
+++ functions/graphFB.js
@@ -1,24 +1,24 @@
-//var graph = require('fbgraph');
-const FacebookGraph = require('facebookgraph');
+var graph = require('fbgraph');
+//const FacebookGraph = require('facebookgraph');
 const access_token = 'DQVJ1c0hlREl1YlQ5ZA1RocmZAzZAXZAQOVc5LVFjdDFIeTZAlY2ZAia1E4VFFWcXo1ajM3RnpfQmNoYm9vajljdDlZAei16ZAGpMVHd1eHNkaVhkVkdzZAGVBbDJhM3JWa3YyMVRRVTdlRnVUUkdrTkNJVjhXaDNhM1l5T0xuQ2VwMTJMam1FbU85YlBKSm5CQjhQdG1uOW5ZAVV8yWnUtVVU4R3ljSkFVelp4V3B3ZAlMtUXk2NXVLWERaQ2x6Y0Ffd0xXQURhZATd4azdRYkFoNlk3QVN5agZDZD';
 //const access_token = 'DQVJ2NUhWeU1kOW5YT0xEQUFYdkkyR3pUUVgydFhieFUxaXpoa3ZAvbERaOXYxNHcxa2Rmbm5tWmRjMm1veVhRWC1Db1hablN2ek1mMlI4dXhzNlJ6ZATZAyV3p4bllMcDZAXM3BkUW02VmhxbEVGV2tmSnhoQ2o5YmdlSUZAjdW9sM0xkMS10NllHZAzdfWExidGJDUnhjMEN6UFVLSjU1ZA29uNnlsbGduRjYzS3RFZAzFxYkhLUDFKRWJQTWRvWndGNmtteUZA4eUpsNEp1RWdSckZAPMQZDZD';
-//graph.setAccessToken(access_token);
-const graph = new FacebookGraph(access_token)
+graph.setAccessToken(access_token);
+//const graph = new FacebookGraph(access_token)
 
 module.exports = {
 	async graphID(id){	
 		console.log(id)
-		const zuck = await graph.get(id+"?fields=name,email");
+		/*const zuck = await graph.get(id+"?fields=name,email");
 		if(zuck==undefined){
 			let error = {name:id,email:id+'@pichincha.com'}
 			return error;
 		}else{
 			return zuck;
 		}
-		//	return zuck;
-			/*graph.get(id+"?fields=name,email", function(err, res){
+*/		//	return zuck;
+			graph.get(id+"?fields=name,email", function(err, res){
 				console.log(res.name);
 				console.log(res.email)
-			});*/
-	}
+			});
+	//}
 };
