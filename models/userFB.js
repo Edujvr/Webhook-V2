21c42be4a58a158edef26bbdfe8bfeb9f9696b06
Edujvr <32462670+Edujvr@users.noreply.github.com>
2021-01-20 15:13:16 -0500

    Create userFB.js

---
+++ models/userFB.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userFBSchema = new Schema({
+  NOMBRE:{
+        type: String,
+        required:true
+  },
+  MAIL{
+    type: String,
+    required:true
+  },
+  ID: {
+    type: String,
+    required:true
+  }
+});
+
+module.exports = mongoose.model("userFB", userFBSchema);
